0b33ec48eb0815379822c37101f5b749d5a56539
Philip Schwartz <phschwartz@phschwartz-t5500.(none)>
2011-12-08 17:09:18 -0500

    WIP more hpcc cli option building.

--- hpcc/hpcc.cpp
+++ hpcc/hpcc.cpp
@@ -26,53 +26,115 @@ enum CLIOptionLevel
 
 interface ICLIOption : extends IInterface
 {
-
+    virtual const char* getDescription() = 0;
+    virtual const char* getKey() = 0;
+    virtual CLIOptionLevel getLevel() = 0;
+    virtual void setDescription(const char*) = 0;
+    virtual void setKey(const char*) = 0;
+    virtual void setLevel(CLIOptionLevel) = 0;
 };
 
 class CCLIOption : public CInterface, implements ICLIOption
 {
 public:
     IMPLEMENT_IINTERFACE;
+
+
+    const char* getDescription()
+    {
+        return description;
+    }
+
+    void setDescription(const char* desc)
+    {
+        description = desc;
+    }
+
+    const char* getKey()
+    {
+        return key;
+    }
+
+    void setKey(const char* _key)
+    {
+        key = _key;
+    }
+
+    CLIOptionLevel getLevel()
+    {
+        return level;
+    }
+
+    void setLevel(CLIOptionLevel _level)
+    {
+        level = _level;
+    }
+
+protected:
+    const char* key;
+    const char* description;
+    CLIOptionLevel level;
+
 };
 
 template <typename T>
 class TCLIOption : public CCLIOption
 {
 public:
-    pair<char*, T> get()
+    /*
+    TCLIOption(T &_value, const char* _key, const char* _description, T _defaultValue=null, CLIOptionLevel _level=CLIGlobal):
+        value(_value), key(_key), description(_description), defaultValue(_defaultValue), level(_level)
     {
-        return pair<char*, T>(key,value);
+
+    }
+    */
+    TCLIOption(const char* _key, const char* _description, CLIOptionLevel _level=CLIGlobal)
+    {
+        key = _key;
+        description = _description;
+        level = _level;
+    }
+
+    void set(pair<const char*, T> kvpair)
+    {
+        key = kvpair.first();
+        value = kvpair.value();
+    }
+
+    pair<const char*, T> get()
+    {
+        if(!value)
+        {
+            value = defaultVal;
+        }
+        return pair<const char*, T>(key,value);
     }
 
 private:
-    T value;
-    char* key;
-    char* description;
+    T *value;
     T defaultVal;
-    CLIOptionLevel level;
-
 };
 
 class CCLIOptionFactory
 {
 public:
-    static CCLIOption* getCLIOption(CLIOptionType type)
+    static CCLIOption* getCLIOption(CLIOptionType type, const char* key, const char* description, CLIOptionLevel level=CLIGlobal)
     {
         switch(type)
         {
-        case CLISTRINGATTR: return new TCLIOption<StringAttr>();
-        case CLIBOOL: return new TCLIOption<bool>();
-        case CLIUNSIGNED: return new TCLIOption<unsigned>();
-        case CLISTRINGBUFFER: return new TCLIOption<StringBuffer>();
+        case CLISTRINGATTR: return new TCLIOption<StringAttr>(key, description, level);
+        case CLIBOOL: return new TCLIOption<bool>(key, description, level);
+        case CLIUNSIGNED: return new TCLIOption<unsigned>(key, description, level);
+        case CLISTRINGBUFFER: return new TCLIOption<StringBuffer>(key, description, level);
         }
     }
 
 };
 
-#define StingAttrOption() CCLIOptionFactory::getCLIOption(CLISTRINGATTR)
-#define BoolOption() CCLIOptionFactory::getCLIOption(CLIBOOL)
-#define UnsignedOption() CCLIOptionFactory::getCLIOption(CLIUNSIGNED)
-#define StingBufferOption() CCLIOptionFactory::getCLIOption(CLISTRINGBUFFER)
+#define StingAttrOption(x,y) CCLIOptionFactory::getCLIOption(CLISTRINGATTR,x,y)
+#define BoolOption(x,y) CCLIOptionFactory::getCLIOption(CLIBOOL,x,y)
+#define UnsignedOption(x,y) CCLIOptionFactory::getCLIOption(CLIUNSIGNED,x,y)
+#define StingBufferOption(x,y) CCLIOptionFactory::getCLIOption(CLISTRINGBUFFER,x,y)
 
 
 class CLIChain
@@ -90,5 +152,7 @@ class CLI
 
 int main(int argc, const char** argv)
 {
+    CCLIOption *opt = BoolOption("OPT1", "This is a test.");
+    cout<<"OPT: "<<opt->getKey()<<" - "<<opt->getDescription()<<" - L:"<<opt->getLevel()<<endl;
     return 0;
 }
